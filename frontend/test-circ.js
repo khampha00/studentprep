a4037617e767d3677c68b59160a8541fd5c7c6a6
aminutukur00 <aminu.tukur@student.aiu.edu.my>
2026-09-04 00:05:19 +0800

    fix: remove global .mord font override to fix squished KaTeX degree symbols

---
+++ frontend/test-circ.js
@@ -0,0 +1,15 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkMath from 'remark-math';
+import remarkRehype from 'remark-rehype';
+import rehypeKatex from 'rehype-katex';
+import rehypeStringify from 'rehype-stringify';
+
+unified()
+  .use(remarkParse)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeKatex)
+  .use(rehypeStringify)
+  .process('$ 60^\\circ $')
+  .then((file) => console.log(String(file)));
